465f7b8185093be5038af37bcad5c17fbdef6222
tallnuts <asteinberg7@gmail.com>
2019-07-18 02:08:27 -0400

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -28,7 +28,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = "=`Result = ${response['result']}`;  
+      el("result-label").innerHTML =`Result = ${response['result']}`;  
       var resp = parseFloat(response);
         if (resp > 0.9){
          el("result-label").innerHTML = "You've got yourself a hotdog!";
