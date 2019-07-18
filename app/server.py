6ce5a985bcd1f49d247d1a924ddd5f8b4d1e0a83
tallnuts <asteinberg7@gmail.com>
2019-07-18 00:59:15 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -63,10 +63,10 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     pred_class,pred_idx,outputs = learn.predict(img)[0]
-    pred=(outputs[0].item())
-    pred=pred*100
-    pred1=round(pred,2)
-    prediction=str(pred1)
+    pred = (outputs[0].item())
+    pred2 = pred*100
+    pred1 = round(pred2,2)
+    prediction = str(pred1)
     responsedata = {'result': str(prediction)}
     return JSONResponse(responsedata)
 
