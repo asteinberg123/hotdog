de68b53b9be0d07dd368785d6e9ae242cbc8ad87
tallnuts <asteinberg7@gmail.com>
2019-07-17 20:41:42 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -62,8 +62,10 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    prediction,outputs = learn.predict(img)[0]
+    return JSONResponse({'result':str(outputs[0])}]
+
+    #return JSONResponse({'result': str(prediction)})
 
 
 if __name__ == '__main__':
