6d0fe2773d9532d49be5218c156c201fc227f7c3
tallnuts <asteinberg7@gmail.com>
2019-07-18 02:53:53 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -60,7 +60,7 @@ async def homepage(request):
 async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
-#     img = open_image(BytesIO(img_bytes))
+    img = open_image(BytesIO(img_bytes))
 #     pred_class,pred_idx,outputs = learn.predict(img)[0]
 #     pred = (outputs[0].item())
 #     pred2 = pred*100
