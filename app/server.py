04e5c89260ac68e3eb25d005b49e2360b3bf001a
tallnuts <asteinberg7@gmail.com>
2019-07-19 18:56:48 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -58,23 +58,23 @@ async def homepage(request):
 
 # @app.route('/analyze', methods=['POST'])
 # async def analyze(request):
-#     img_data = await request.form()
-#     img_bytes = await (img_data['file'].read())
-#     img = open_image(BytesIO(img_bytes))
-#     pred_class,pred_idx,outputs = learn.predict(img)[0]
-#     pred = (outputs[0].item())
-#     pred2 = pred*100
-#     pred1 = round(pred2,2)
-#     prediction = str(pred1)
-#     responsedata = {'result': str(prediction)}
-#     return JSONResponse(responsedata)
-@app.route('/analyze', methods=['POST'])
-async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    pred_class,pred_idx,outputs = learn.predict(img)[0]
+    pred = (outputs[0].item())
+    pred2 = pred*100
+    pred1 = round(pred2,2)
+    prediction = str(pred1)
+    responsedata = {'result': str(prediction)}
+    return JSONResponse(responsedata)
+# @app.route('/analyze', methods=['POST'])
+# async def analyze(request):
+#     img_data = await request.form()
+#     img_bytes = await (img_data['file'].read())
+#     img = open_image(BytesIO(img_bytes))
+#     prediction = learn.predict(img)[0]
+#     return JSONResponse({'result': str(prediction)})
 
 
 if __name__ == '__main__':
