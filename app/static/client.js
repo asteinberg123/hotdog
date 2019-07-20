edc2da8a4a8fbc23fe083005d45fca0c2c1250c2
tallnuts <asteinberg7@gmail.com>
2019-07-19 22:27:27 -0400

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -28,18 +28,18 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = response;
-      var respo = `(${response["result"]})`;
-      el("result-label").innerHTML = `Result = respo`;
-      var resp = parseFloat(respo)
-        if (resp > 90.00){
-         el("result-label").innerHTML = "You've got yourself a hotdog!";
-       }else if(resp > 50.00){
-          el("result-label").innerHTML = "I'd say there's about a " + resp + "percent chance of that being a hotdog.";
-        }else if(resp < 20.00){
-         el("result-label").innerHTML = "Are you kidding me?  You thought that was a hotdog?";
-       }else if(resp <= 50.00){
-         el("result-label").innerHTML = "I don't think that's a hotdog.";        
+      el("result-label").innerHTML = `Result = ${response[prediction]}`;
+//       var respo = `(${response["result"]})`;
+//       el("result-label").innerHTML = `Result = respo`;
+//       var resp = parseFloat(respo)
+//         if (resp > 90.00){
+//          el("result-label").innerHTML = "You've got yourself a hotdog!";
+//        }else if(resp > 50.00){
+//           el("result-label").innerHTML = "I'd say there's about a " + resp + "percent chance of that being a hotdog.";
+//         }else if(resp < 20.00){
+//          el("result-label").innerHTML = "Are you kidding me?  You thought that was a hotdog?";
+//        }else if(resp <= 50.00){
+//          el("result-label").innerHTML = "I don't think that's a hotdog.";        
            
     }
     el("analyze-button").innerHTML = "Analyze";
