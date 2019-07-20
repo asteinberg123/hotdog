76cc7b99eca9f2b5f5b7e665639419e19d5c9fa3
tallnuts <asteinberg7@gmail.com>
2019-07-20 00:10:03 -0400

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,7 @@ function analyze() {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       //el("result-label").innerHTML = `Result = ${response["result"]}`;
-      //`var respo = ${response["result"]}`;
+      var respo = `${response["result"]}`;
       //el("result-label").innerHTML = `Result = respo`;
       var resp = parseFloat(response);
         if (resp > 90.00){
